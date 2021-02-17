4efc435bc15e75b3195df4ab0576d4fa19db4a12
walkinglele <leleberjalan@gmail.com>
2021-02-17 10:59:36 +0700

    hari ke 3

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -10,14 +10,28 @@ import SwiftUI
 struct ContentView: View {
   
     @Environment(\.managedObjectContext) var managedObjectContext
+  
+  @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
     
     @State private var showingAddTodoView: Bool = false
     var body: some View {
         NavigationView{
-            List(0..<5){ item in
-                Text("Hello World")
-            }.navigationBarTitle("Todo",displayMode: .inline)
-            .navigationBarItems(trailing:
+          List{
+            ForEach(self.todos, id: \.self){ todo in
+              HStack{
+                Text(todo.name ?? "Unknown")
+                
+                Spacer()
+                
+                Text(todo.priority ?? "Unknown")
+              }
+            }
+            .onDelete(perform: deleteTodo)
+          }
+            .navigationBarTitle("Todo",displayMode: .inline)
+            .navigationBarItems(
+              leading: EditButton(),
+              trailing:
                                     Button(action: {
                                         self.showingAddTodoView.toggle()
                                         
@@ -30,10 +44,25 @@ struct ContentView: View {
                                     )
         }
     }
+  
+  private func deleteTodo(at offsets: IndexSet){
+    for index in offsets{
+      let todo = todos[index]
+      managedObjectContext.delete(todo)
+      
+      do{
+        try managedObjectContext.save()
+      } catch{
+        print(error)
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+      return ContentView()
+        .environment(\.managedObjectContext, context)
     }
 }
